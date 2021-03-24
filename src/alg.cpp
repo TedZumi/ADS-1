196b81d41c359fe549c0c025a48748a83af04f4a
TedZumi <79110264+TedZumi@users.noreply.github.com>
2021-03-24 15:38:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,33 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int i = 0, j = size - 1, k = 0;
+  while(i < j){
+    int mid = i + (j - i) / 2;
+    if (arr[mid] == value){
+      k += 1;
+      int h = 1;
+      while (arr[mid - h] == value){
+        k += 1;
+        h += 1;
+      }
+      h = 1;
+      while (arr[mid + h] == value){
+        k += 1;
+        h += 1;
+      }
+      return k;
+    }
+    else if (arr[mid] > value)
+      j = mid;
+    else
+      i = mid + 1;
+    if (j == i){
+      if (arr[i] == value){
+        k += 1;
+        return k;
+      }
+    }
+  }
+  return 0;
 }
