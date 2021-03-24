1407141de9aed680463e6c08ba1af536779fac54
TedZumi <79110264+TedZumi@users.noreply.github.com>
2021-03-24 15:54:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,28 +2,27 @@
 
 int cbinsearch(int *arr, int size, int value) {
   int i = 0, j = size - 1, k = 0;
-  while(i < j){
+  while (i < j) {
     int mid = i + (j - i) / 2;
-    if (arr[mid] == value){
+    if (arr[mid] == value) {
       k += 1;
       int h = 1;
-      while (arr[mid - h] == value){
+      while (arr[mid - h] == value) {
         k += 1;
         h += 1;
       }
       h = 1;
-      while (arr[mid + h] == value){
+      while (arr[mid + h] == value) {
         k += 1;
         h += 1;
       }
       return k;
-    }
-    else if (arr[mid] > value)
+    } else if (arr[mid] > value)
       j = mid;
     else
       i = mid + 1;
-    if (j == i){
-      if (arr[i] == value){
+    if (j == i) {
+      if (arr[i] == value) {
         k += 1;
         return k;
       }
